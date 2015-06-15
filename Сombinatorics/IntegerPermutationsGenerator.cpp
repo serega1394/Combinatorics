dbb24cba3cf237aa7aa34362194c824a282c9f82
Sergey Rozhkov <progger007@gmail.com>
2015-06-15 23:34:39 +0300

    Добавлены методы нахождения перестановки по номеру и наоборот в класс IntegerPermutationsGenerator

--- Сombinatorics/IntegerPermutationsGenerator.cpp
+++ Сombinatorics/IntegerPermutationsGenerator.cpp
@@ -34,3 +34,56 @@ void IntegerPermutationsGenerator::toFile(std::string filename)
 {
 	CombinatoricsOutput<int>::toFile(filename, _result);
 }
+
+int IntegerPermutationsGenerator::getPermutationNumber(std::vector<int> permutation)
+{
+	int number = 1;
+	for (int i = 1; i < permutation.size(); i++)
+	{
+		number += permutation[i - 1] * factorial(permutation.size() - i);
+
+		if (permutation[i - 1] != permutation.size() - i + 1)
+		{
+			for (int j = i; j > permutation.size(); j--)
+			{
+				permutation[j]--;
+			}
+		}
+
+	}
+
+	return number;
+}
+
+std::vector<int> getPermutation(int number, int n)
+{
+	std::vector<int> result;
+	bool *isUse = new bool[n];
+	for (int i = 0; i < n; i++)
+	{
+		isUse[i] = false;
+	}
+
+	int i = 1;
+	for (; i < n && number % factorial(n - i) + 1 != 0; i++)
+	{
+		int item = number / factorial(n - i) + 1;
+		//if ()
+		result.push_back(item);
+		isUse[item - 1] = true;
+	}
+	
+	result.push_back(number / factorial(n - i));
+	return result;
+}
+
+unsigned int factorial(unsigned int a)
+{
+	// факториалы от 0 до 10 уже определены в библиотеке для повышения эффективвности работы
+	if (a <= MAX_SIZE)
+	{
+		return FACTORIAL[a];
+	}
+	
+	throw MAX_SIZE_EXCEPTION;
+}
